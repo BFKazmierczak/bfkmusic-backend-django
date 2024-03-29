d6cd2307f8b53a6d0ba5f29f75bff97f40f0e0b5
BFKazmierczak <bfkazmierczak@gmail.com>
2024-03-30 00:27:09 +0100

    add graphene

---
+++ bfkmusic/polls/schema.py
@@ -0,0 +1,38 @@
+import graphene
+from graphene_django import DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
+from django_filters import FilterSet, CharFilter
+
+from polls.models import Choice, Question
+
+
+class ChoiceType(DjangoObjectType):
+    class Meta:
+        model = Choice
+        interfaces = (graphene.relay.Node,)
+        exclude = ()
+
+
+class QuestionType(DjangoObjectType):
+    class Meta:
+        model = Question
+        interfaces = (graphene.relay.Node,)
+        exclude = ()
+
+
+class QuestionFilter(FilterSet):
+    name = CharFilter(lookup_expr=["iexact"])
+
+    class Meta:
+        model = Question
+        fields = ["id"]
+
+
+class Query(graphene.ObjectType):
+    all_questions = DjangoFilterConnectionField(
+        QuestionType, filterset_class=QuestionFilter
+    )
+    all_choices = DjangoFilterConnectionField(ChoiceType)
+
+
+schema = graphene.Schema(query=Query)
