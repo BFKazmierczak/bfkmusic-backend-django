10095f0a5cb44d7b42fead85ce5cef8fafec88de
BFKazmierczak <bfkazmierczak@gmail.com>
2024-04-04 15:57:09 +0200

    feat: add duration and waveform calculation

---
+++ core_api/audio_processing/waveform_extractor.py
@@ -0,0 +1,25 @@
+import numpy as np
+import librosa
+
+
+def extract_peaks(audio_path: str):
+
+    y, sr = librosa.load(audio_path, sr=None)
+
+    duration = librosa.get_duration(y=y, sr=sr)
+
+    samples_per_second = sr
+
+    samples_per_window = samples_per_second
+
+    num_windows = len(y) // samples_per_window
+
+    amplitudes = []
+
+    for i in range(num_windows):
+        start_sample = i * samples_per_window
+        end_sample = start_sample + samples_per_window
+        amplitude = np.mean(np.abs(y[start_sample:end_sample]))
+        amplitudes.append(str(amplitude))
+
+    return amplitudes, duration
--- core_api/signals/signals.py
+++ core_api/signals/signals.py
@@ -1,10 +1,24 @@
-from django.db.models.signals import pre_save
+import json
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from core_api.audio_processing.waveform_extractor import extract_peaks
 from core_api.models import Audio
 
+from django.conf import settings
 
-@receiver(pre_save, sender=Audio)
-def process_audio(sender, instance, **kwargs):
-    if instance.id is None:
-        print("audio created")
+from django.db.models.fields.files import FieldFile
+
+
+@receiver(post_save, sender=Audio)
+def process_audio(sender, instance: Audio, created, **kwargs):
+    if created:
+        file: FieldFile = instance.file
+
+        amplitudes, duration = extract_peaks(file.path)
+
+        json_amplitudes = json.dumps(amplitudes)
+
+        instance.waveform = json_amplitudes
+        instance.duration = duration
+        instance.save()
